5692f53572210c4180c761e684e551bb915eef78
Andres Manco <andresmanco@gmail.com>
2018-06-14 15:21:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,8 @@ function nowServing(line){
   if(line.length===0){
     return "There is nobody waiting to be served!";
   }
-  else{
-    return `Currently serving ${line.shift()}.`;
-  }
+ return `Currently serving ${line.shift()}.`;
+  
 }
 
 function currentLine(line){
