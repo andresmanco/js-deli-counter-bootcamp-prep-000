5d0aa68dc91a09622b12e81c8773939909d33c16
Andres Manco <andresmanco@gmail.com>
2018-06-14 15:28:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,20 +11,18 @@ function nowServing(line){
     return "There is nobody waiting to be served!";
   }
   return `Currently serving ${line.shift()}.`;
-  
 }
 
 function currentLine(line){
   console.log(line);
-  var n = 1;
   var st ="The line is currently: "
   if(line.length > 0){
     for(var i = 0; i<line.length; i++){
       if(i === line.length-1){
-        st =`${st}${n++}. ${line[i]}`;
+        st =`${st}${i+1}. ${line[i]}`;
       }
       else{
-        st =`${st}${n++}. ${line[i]}, `;
+        st =`${st}${}. ${line[i]}, `;
       }
     }
     return st;
